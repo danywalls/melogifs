9dd1bc2ebc04dc10115a5bf6ee4388081e6293d8
Dany Paredes <danywalls4@gmail.com>
2020-04-17 23:30:36 +0200

    add key

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [gifs, setGifs] = useState([
-    "https://media1.giphy.com/media/fAQHjEYDT9GweWIcBq/giphy.gif?cid=ecf05e474c6593ae881a2d64be691d80cfc16d637bfcf78d&rid=giphy.gif",
-    "https://media2.giphy.com/media/gfGvtlxqgYKIEqSEdE/giphy.gif",
-  ]);
+  const urlAPI =
+    "https://api.giphy.com/v1/gifs/search?api_key=fdrhC4K8gCbiL9nGu0MlKblnRYbS3IEW&q=carmelo&limit=25&offset=0&rating=G&lang=en";
+  const [gifs, setGifs] = useState([]);
 
   const ALTER_GIFS = [
     "https://media1.giphy.com/media/ZZTC9RP7cVYqw5MnXW/giphy.gif",
     "https://media2.giphy.com/media/mED0gOvnbDojRp2VMI/giphy.gif",
   ];
-
+  useEffect(() => {
+    fetch(urlAPI)
+      .then((res) => res.json())
+      .then((response) => {
+        const { data } = response;
+        const gifs = data.map((image) => image.images.downsized_medium.url);
+        setGifs(gifs);
+      });
+  }, []);
   return (
     <div className="App">
       {gifs.map((gif) => (
         <img src={gif} alt={gif} />
       ))}
-      <button onClick={() => setGifs(ALTER_GIFS)}>Change </button>
+      {/* <button onClick={() => setGifs(ALTER_GIFS)}>Change </button> */}
     </div>
   );
 }
