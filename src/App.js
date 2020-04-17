574c2a236cbb65a9c010b81b88cc08b5ba2af66f
Dany Paredes <danywalls4@gmail.com>
2020-04-17 23:05:51 +0200

    remove no items not used.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [gifs, setGifs] = useState([
+    "https://media1.giphy.com/media/fAQHjEYDT9GweWIcBq/giphy.gif?cid=ecf05e474c6593ae881a2d64be691d80cfc16d637bfcf78d&rid=giphy.gif",
+    "https://media2.giphy.com/media/gfGvtlxqgYKIEqSEdE/giphy.gif",
+  ]);
+
+  const ALTER_GIFS = [
+    "https://media1.giphy.com/media/ZZTC9RP7cVYqw5MnXW/giphy.gif",
+    "https://media2.giphy.com/media/mED0gOvnbDojRp2VMI/giphy.gif",
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {gifs.map((gif) => (
+        <img src={gif} alt={gif} />
+      ))}
+      <button onClick={() => setGifs(ALTER_GIFS)}>Change </button>
     </div>
   );
 }
